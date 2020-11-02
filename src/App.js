3a27b119e71c09735a4474e2319a5de7a4b3b35c
reyhanfebrizioresman <reyhanfebrizioresman12@gmail.com>
2020-11-03 01:10:28 +0700

    filter-component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import data from "./data.json";
 import Products from "./components/Products";
+import Filter from "./components/Filter";
 
 class App extends React.Component {
   constructor(){
@@ -12,6 +13,42 @@ class App extends React.Component {
       sort: "",
     };
   }
+  sortProducts = (event) => {
+    // Implementasi
+    const sort = event.target.value;
+    console.log(event.target.value);
+    this.setState((state) => ({
+      sort: event.target.value,
+      products : this.state.products.slice().sort((a,b) =>
+        sort === "lowest"
+        ? a.price > b.price
+            ? 1
+            : -1
+        : sort === "highest"
+        ? a.price < b.price 
+            ? 1
+            : -1
+          : a._id < b._id 
+            ? 1
+            :-1
+      ),
+    }));
+  };
+  filterProducts = (event) =>{
+    // Implementasi
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products})
+    }else{
+      this.setState({
+        size: event.target.value,
+        // Menggunakan data products dari import Products from "./components/Products";
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  }
   render(){
   return (
     <div className="grid-container">
@@ -19,9 +56,15 @@ class App extends React.Component {
         <a  href="/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
         <div className="content">
           <div className="main">
+            {/* Membuat Properti untuk menguitung jumlah */}
+            <Filter count={this.state.products.length}
+            size={this.state.size}
+            sort={this.state.sort}
+            filterProducts={this.filterProducts}
+            sortProducts={this.sortProducts}
+            ></Filter>
             <Products products={this.state.products}></Products>
           </div>
           <div className="sidebar">Cart Items</div>
